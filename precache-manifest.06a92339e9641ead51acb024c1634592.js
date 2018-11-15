758987e89efe0f54b3df6bb55a9f6f38c7cd78dd
Tish <latisha.griffiths@gmail.com>
2018-11-15 10:17:30 -0600

    Updates

---
+++ precache-manifest.06a92339e9641ead51acb024c1634592.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "589bf33a4d4a4c11cbf075e0a90d4d49",
+    "url": "/web-jams-react/static/media/header-img.589bf33a.jpg"
+  },
+  {
+    "revision": "ff758b3f1ed2f83c519f",
+    "url": "/web-jams-react/static/js/runtime~main.ff758b3f.js"
+  },
+  {
+    "revision": "3ff279ee07e6a173848b",
+    "url": "/web-jams-react/static/js/main.3ff279ee.chunk.js"
+  },
+  {
+    "revision": "b87ba9243393115c9d82",
+    "url": "/web-jams-react/static/js/1.b87ba924.chunk.js"
+  },
+  {
+    "revision": "3ff279ee07e6a173848b",
+    "url": "/web-jams-react/static/css/main.5ab180c1.chunk.css"
+  },
+  {
+    "revision": "b87ba9243393115c9d82",
+    "url": "/web-jams-react/static/css/1.c1700c80.chunk.css"
+  },
+  {
+    "revision": "eb59979cd41412c7089e40c74c87df35",
+    "url": "/web-jams-react/index.html"
+  }
+];
